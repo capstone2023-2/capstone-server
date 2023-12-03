487cc298be73a4709ee4a1a71e215e108a7bf296
Flash-KIM142 <topqr123q@gmail.com>
2023-12-03 21:08:20 +0900

    remove: 사용하지 않는 기능 삭제

--- src/main/java/com/capstone/demo/controller/UserController.java
+++ src/main/java/com/capstone/demo/controller/UserController.java
@@ -1,7 +1,5 @@
 package com.capstone.demo.controller;
 
-import com.capstone.demo.model.dto.request.LoginRequest;
-import com.capstone.demo.model.dto.request.UserRegisterDto;
 import com.capstone.demo.model.dto.response.BaseResponseDto;
 import com.capstone.demo.model.dto.response.UserResponseDto;
 import com.capstone.demo.service.UserService;
@@ -17,8 +15,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,25 +25,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     private final UserService userService;
-
-    @Operation(summary = "회원가입", description = "이메일(email), 비밀번호(password)와 사용자 이름(username)을 이용하여 회원가입을 합니다."
-            , tags = "사용자 기능")
-    @PostMapping("/join")
-    public ResponseEntity<String> join(@RequestBody UserRegisterDto userRegisterDto) {
-
-        userService.join(userRegisterDto);
-        return ResponseEntity.ok().body("user successfully registered!");
-    }
-
-    @Operation(summary = "로그인", description = "이메일(email)과 비밀번호(password)를 이용하여 로그인을 합니다.", responses = {
-            @ApiResponse(description = "로그인 성공 -> 토큰 반환")
-    }, tags = "사용자 기능")
-    @PostMapping("/login")
-    public ResponseEntity<String> login(@RequestBody LoginRequest loginRequest) {
-
-        String token = userService.login(loginRequest);
-        return ResponseEntity.ok().body(token);
-    }
 
     @Operation(summary = "사용자 조회", description = "한 명의 사용자 정보를 조회합니다.", responses = {
             @ApiResponse(
